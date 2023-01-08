948d4915720881c0b49e86cd4d46f96cae7fe851
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2023-01-08 16:20:03 -0500

    adjust js file structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const navButton = document.querySelector('#nav-btn');
-const nav = document.querySelector('#nav-list');
+const navList = document.querySelector('#nav-list');
 const COLOR_RADIOS = document.querySelectorAll('input[type="radio"]');
 
 function toggleNav() {
     navButton.classList.toggle('open');
-    nav.classList.toggle('open');
+    navList.classList.toggle('open');
 }
 
 function driftCloud(e) {
