848f65b077cf893466c9b8275b08d189b266c637
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2023-01-23 19:42:30 -0500

    add theme styles

--- js/main.js
+++ js/main.js
@@ -39,21 +39,44 @@ function toggleTheme(e) {
     console.log(themeSwitch.checked);
 
     if (e.target.checked) {
-        console.log('night')
+        // * NiGHT MODE
+        root.style.setProperty('--cyclone-filter', 'var(--cyclone-filter-night)');
+        root.style.setProperty('--color-text-primary', 'var(--color-text-primary-night)');
         root.style.setProperty('--background', 'var(--background-night)');
+        root.style.setProperty('--footer-text', 'var(--footer-text-night)');
+        root.style.setProperty('--footer-background', 'var(--footer-background-night)');
+        root.style.setProperty('--moon-filter', 'var(--moon-filter-night)');
+        root.style.setProperty('--nav-link-color', 'var(--nav-link-color-night');
+        root.style.setProperty('--theme-switch-border', 'var(--theme-switch-border-night)');
         root.style.setProperty('--shop-background-item', 'var(--shop-background-item-night)');
         root.style.setProperty('--shop-item-background', 'var(--shop-item-background-night)');
         root.style.setProperty('--shop-item-box-shadow', 'var(--shop-item-box-shadow-night)');
         root.style.setProperty('--shop-item-box-shadow-hover', 'var(--shop-item-box-shadow-hover-night)');
-
+        root.style.setProperty('--shop-item-title', 'var(--shop-item-title-night)');
+        root.style.setProperty('--shop-radio-background', 'var(--shop-radio-background-night)'); 
+        root.style.setProperty('--shop-radio-shadow', 'var(--shop-radio-shadow-night)');
+        root.style.setProperty('--shop-radio-shadow-one', 'var(--shop-radio-shadow-one-night)');
+        root.style.setProperty('--shop-radio-shadow-two', 'var(--shop-radio-shadow-two-night)');
+        
     } else if (!e.target.checked) {
-        console.log('light');
+        // * DAY MODE 
+        root.style.setProperty('--cyclone-filter', 'var(--cyclone-filter-day)');
+        root.style.setProperty('--color-text-primary', 'var(--color-text-primary-day)');
         root.style.setProperty('--background', 'var(--background-day)');
+        root.style.setProperty('--footer-text', 'var(--footer-text-day)');
+        root.style.setProperty('--footer-background', 'var(--footer-background-day)');
+        root.style.setProperty('--moon-filter', 'var(--moon-filter-day)');
+        root.style.setProperty('--nav-link-color', 'var(--nav-link-color-day');
+        root.style.setProperty('--theme-switch-border', 'var(--theme-switch-border-day)');
         root.style.setProperty('--shop-background-item', 'var(--shop-background-item-day)');
         root.style.setProperty('--shop-item-background', 'var(--shop-item-background-day)');
         root.style.setProperty('--shop-item-box-shadow', 'var(--shop-item-box-shadow-day)');
         root.style.setProperty('--shop-item-box-shadow-hover', 'var(--shop-item-box-shadow-hover-day)');
-
+        root.style.setProperty('--shop-item-title', 'var(--shop-item-title-day)');
+        root.style.setProperty('--shop-radio-background', 'var(--shop-radio-background-day)'); 
+        root.style.setProperty('--shop-radio-shadow', 'var(--shop-radio-shadow-day)');
+        root.style.setProperty('--shop-radio-shadow-one', 'var(--shop-radio-shadow-one-day)');
+        root.style.setProperty('--shop-radio-shadow-two', 'var(--shop-radio-shadow-two-day)');
     }
 }
 
