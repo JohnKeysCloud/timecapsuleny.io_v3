b2a5ee799abf49af27e1f49c0e3cab6cc86ebe06
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2023-02-23 23:39:43 -0500

    adjust regex to support safari

--- js/shop.js
+++ js/shop.js
@@ -2,14 +2,15 @@ const COLOR_RADIOS = document.querySelectorAll('input[type="radio"]');
 
 function switchItemColor(e) {
     let radioValue = e.target.value;
-    let itemColorRegEx = /(?<=\[)(.*?)(?=\])/g;
-
+    let itemColorRegEx = /\[(.*?)\]/g;
+    
     let closestItemImage = e.target.closest('.item-card').querySelector('.item-img');
     let currentItemColor = closestItemImage.src.match(itemColorRegEx)[0];
+    console.log(currentItemColor);
 
     if (radioValue === currentItemColor) return; 
     
-    let newImageSource = closestItemImage.getAttribute('src').replaceAll(itemColorRegEx, radioValue);
+    let newImageSource = closestItemImage.getAttribute('src').replaceAll(itemColorRegEx, `[${radioValue}]`);
     
     closestItemImage.setAttribute('src', newImageSource);
 }
