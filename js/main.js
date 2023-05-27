dcd72d496f85ee343904f1acab39b8262300921e
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2023-05-27 15:27:00 -0400

    reformat code

--- js/main.js
+++ js/main.js
@@ -11,116 +11,211 @@ let cloudState = 'mobile';
 let nightMode = localStorage.getItem('nightMode');
 
 function remobilizeCloud() {
-    if (root.clientWidth < 768 && cloudState === 'mobile') return;
-    if (navButton.classList.contains('open')) navButton.click();
+  if (root.clientWidth < 768 && cloudState === 'mobile') return;
+  if (navButton.classList.contains('open')) navButton.click();
 }
 
 function driftCloud(e) {
-    const clientX = e.clientX;
-    const clientY = e.clientY;  
-    
-    let halfCloudEncapsulatorHeight = cloudEncapsulator.offsetHeight / 2;
-    let halfCloudEncapsulatorWidth = cloudEncapsulator.offsetWidth / 2;
+  const clientX = e.clientX;
+  const clientY = e.clientY;
 
-    cloudOne.style.transform = `translate(${clientX - halfCloudEncapsulatorWidth}px, ${clientY - halfCloudEncapsulatorHeight}px)`;
+  let halfCloudEncapsulatorHeight = cloudEncapsulator.offsetHeight / 2;
+  let halfCloudEncapsulatorWidth = cloudEncapsulator.offsetWidth / 2;
+
+  cloudOne.style.transform = `translate(${
+    clientX - halfCloudEncapsulatorWidth
+  }px, ${clientY - halfCloudEncapsulatorHeight}px)`;
 }
 
 function toggleNav() {
-    navButton.classList.toggle('open');
-    navList.classList.toggle('open');
+  navButton.classList.toggle('open');
+  navList.classList.toggle('open');
 
-    if (navList.classList.contains('open')) {
-        window.removeEventListener('mousemove', driftCloud);
-        cloudState = 'immobile';
-    } else {
-        window.addEventListener('mousemove', driftCloud);
-        cloudState = 'mobile';
-    }
+  if (navList.classList.contains('open')) {
+    window.removeEventListener('mousemove', driftCloud);
+    cloudState = 'immobile';
+  } else {
+    window.addEventListener('mousemove', driftCloud);
+    cloudState = 'mobile';
+  }
 }
 
 function setLocalStorage() {
-    nightMode = localStorage.getItem('nightMode');
+  nightMode = localStorage.getItem('nightMode');
 
-    if (themeSwitch.checked) {
-        localStorage.setItem('nightMode', 'enabled');
-    } else if (!themeSwitch.checked) {
-        localStorage.setItem('nightMode', null);
-    }
+  if (themeSwitch.checked) {
+    localStorage.setItem('nightMode', 'enabled');
+  } else if (!themeSwitch.checked) {
+    localStorage.setItem('nightMode', null);
+  }
 }
 
 function toggleTheme() {
-    if (themeSwitch.checked) {
-        // * NiTE MODE
-        root.style.setProperty('--cyclone-filter', 'var(--cyclone-filter-night)');
-        root.style.setProperty('--cloud-color', 'var(--cloud-color-night)');
-        root.style.setProperty('--moon-filter', 'var(--moon-filter-night)');
+  if (themeSwitch.checked) {
+    // * NiTE MODE
+    root.style.setProperty('--cyclone-filter', 'var(--cyclone-filter-night)');
+    root.style.setProperty('--cloud-color', 'var(--cloud-color-night)');
+    root.style.setProperty('--moon-filter', 'var(--moon-filter-night)');
 
-        root.style.setProperty('--color-text-primary', 'var(--color-text-primary-night)');
-        root.style.setProperty('--background', 'var(--background-night)');
-        root.style.setProperty('--theme-switcher-background', 'var(--theme-switcher-background-night)');
-        root.style.setProperty('--theme-switcher-box-shadow', 'var(--theme-switcher-box-shadow-night)');
+    root.style.setProperty(
+      '--color-text-primary',
+      'var(--color-text-primary-night)'
+    );
+    root.style.setProperty('--background', 'var(--background-night)');
+    root.style.setProperty(
+      '--theme-switcher-background',
+      'var(--theme-switcher-background-night)'
+    );
+    root.style.setProperty(
+      '--theme-switcher-box-shadow',
+      'var(--theme-switcher-box-shadow-night)'
+    );
 
-        root.style.setProperty('--nav-button-color', 'var(--nav-button-color-night)');
-        root.style.setProperty('--nav-button-shadow', 'var(--nav-button-shadow-night)');
-        root.style.setProperty('--nav-link-color', 'var(--nav-link-color-night');
-        root.style.setProperty('--nav-link-background-mobile', 'var(--nav-link-background-mobile-night');
+    root.style.setProperty(
+      '--nav-button-color',
+      'var(--nav-button-color-night)'
+    );
+    root.style.setProperty(
+      '--nav-button-shadow',
+      'var(--nav-button-shadow-night)'
+    );
+    root.style.setProperty('--nav-link-color', 'var(--nav-link-color-night');
+    root.style.setProperty(
+      '--nav-link-background-mobile',
+      'var(--nav-link-background-mobile-night'
+    );
 
-        root.style.setProperty('--footer-text', 'var(--footer-text-night)');
-        root.style.setProperty('--footer-background', 'var(--footer-background-night)');
+    root.style.setProperty('--footer-text', 'var(--footer-text-night)');
+    root.style.setProperty(
+      '--footer-background',
+      'var(--footer-background-night)'
+    );
 
-        root.style.setProperty('--shop-background-item', 'var(--shop-background-item-night)');
-        root.style.setProperty('--shop-item-background', 'var(--shop-item-background-night)');
-        root.style.setProperty('--shop-item-box-shadow', 'var(--shop-item-box-shadow-night)');
-        root.style.setProperty('--shop-item-box-shadow-hover', 'var(--shop-item-box-shadow-hover-night)');
-        root.style.setProperty('--shop-item-title', 'var(--shop-item-title-night)');
-        root.style.setProperty('--shop-radio-background', 'var(--shop-radio-background-night)'); 
-        root.style.setProperty('--shop-radio-shadow', 'var(--shop-radio-shadow-night)');
-        root.style.setProperty('--shop-radio-shadow-one', 'var(--shop-radio-shadow-one-night)');
-        root.style.setProperty('--shop-radio-shadow-two', 'var(--shop-radio-shadow-two-night)');    
+    root.style.setProperty(
+      '--shop-background-item',
+      'var(--shop-background-item-night)'
+    );
+    root.style.setProperty(
+      '--shop-item-background',
+      'var(--shop-item-background-night)'
+    );
+    root.style.setProperty(
+      '--shop-item-box-shadow',
+      'var(--shop-item-box-shadow-night)'
+    );
+    root.style.setProperty(
+      '--shop-item-box-shadow-hover',
+      'var(--shop-item-box-shadow-hover-night)'
+    );
+    root.style.setProperty('--shop-item-title', 'var(--shop-item-title-night)');
+    root.style.setProperty(
+      '--shop-radio-background',
+      'var(--shop-radio-background-night)'
+    );
+    root.style.setProperty(
+      '--shop-radio-shadow',
+      'var(--shop-radio-shadow-night)'
+    );
+    root.style.setProperty(
+      '--shop-radio-shadow-one',
+      'var(--shop-radio-shadow-one-night)'
+    );
+    root.style.setProperty(
+      '--shop-radio-shadow-two',
+      'var(--shop-radio-shadow-two-night)'
+    );
 
-        root.style.setProperty('--about-main-shadow', ' var(--about-main-shadow-night)');
-    } else if (!themeSwitch.checked) {
-        // * DAY MODE 
-        root.style.setProperty('--cyclone-filter', 'var(--cyclone-filter-day)');
-        root.style.setProperty('--cloud-color', 'var(--cloud-color-day)');
-        root.style.setProperty('--moon-filter', 'var(--moon-filter-day)');
+    root.style.setProperty(
+      '--about-main-shadow',
+      ' var(--about-main-shadow-night)'
+    );
+  } else if (!themeSwitch.checked) {
+    // * DAY MODE
+    root.style.setProperty('--cyclone-filter', 'var(--cyclone-filter-day)');
+    root.style.setProperty('--cloud-color', 'var(--cloud-color-day)');
+    root.style.setProperty('--moon-filter', 'var(--moon-filter-day)');
 
-        root.style.setProperty('--color-text-primary', 'var(--color-text-primary-day)');
-        root.style.setProperty('--background', 'var(--background-day)');
-        root.style.setProperty('--theme-switcher-background', 'var(--theme-switcher-background-day)');
-        root.style.setProperty('--theme-switcher-box-shadow', 'var(--theme-switcher-box-shadow-day)');
+    root.style.setProperty(
+      '--color-text-primary',
+      'var(--color-text-primary-day)'
+    );
+    root.style.setProperty('--background', 'var(--background-day)');
+    root.style.setProperty(
+      '--theme-switcher-background',
+      'var(--theme-switcher-background-day)'
+    );
+    root.style.setProperty(
+      '--theme-switcher-box-shadow',
+      'var(--theme-switcher-box-shadow-day)'
+    );
 
-        root.style.setProperty('--nav-button-color', 'var(--nav-button-color-day)');
-        root.style.setProperty('--nav-button-shadow', 'var(--nav-button-shadow-day)');
-        root.style.setProperty('--nav-link-color', 'var(--nav-link-color-day');
-        root.style.setProperty('--nav-link-background-mobile', 'var(--nav-link-background-mobile-day');
-        
-        root.style.setProperty('--footer-text', 'var(--footer-text-day)');
-        root.style.setProperty('--footer-background', 'var(--footer-background-day)');
+    root.style.setProperty('--nav-button-color', 'var(--nav-button-color-day)');
+    root.style.setProperty(
+      '--nav-button-shadow',
+      'var(--nav-button-shadow-day)'
+    );
+    root.style.setProperty('--nav-link-color', 'var(--nav-link-color-day');
+    root.style.setProperty(
+      '--nav-link-background-mobile',
+      'var(--nav-link-background-mobile-day'
+    );
 
-        root.style.setProperty('--shop-background-item', 'var(--shop-background-item-day)');
-        root.style.setProperty('--shop-item-background', 'var(--shop-item-background-day)');
-        root.style.setProperty('--shop-item-box-shadow', 'var(--shop-item-box-shadow-day)');
-        root.style.setProperty('--shop-item-box-shadow-hover', 'var(--shop-item-box-shadow-hover-day)');
-        root.style.setProperty('--shop-item-title', 'var(--shop-item-title-day)');
-        root.style.setProperty('--shop-radio-background', 'var(--shop-radio-background-day)'); 
-        root.style.setProperty('--shop-radio-shadow', 'var(--shop-radio-shadow-day)');
-        root.style.setProperty('--shop-radio-shadow-one', 'var(--shop-radio-shadow-one-day)');
-        root.style.setProperty('--shop-radio-shadow-two', 'var(--shop-radio-shadow-two-day)');
+    root.style.setProperty('--footer-text', 'var(--footer-text-day)');
+    root.style.setProperty(
+      '--footer-background',
+      'var(--footer-background-day)'
+    );
 
-        root.style.setProperty('--about-main-shadow', ' var(--about-main-shadow-day)');
-    }
+    root.style.setProperty(
+      '--shop-background-item',
+      'var(--shop-background-item-day)'
+    );
+    root.style.setProperty(
+      '--shop-item-background',
+      'var(--shop-item-background-day)'
+    );
+    root.style.setProperty(
+      '--shop-item-box-shadow',
+      'var(--shop-item-box-shadow-day)'
+    );
+    root.style.setProperty(
+      '--shop-item-box-shadow-hover',
+      'var(--shop-item-box-shadow-hover-day)'
+    );
+    root.style.setProperty('--shop-item-title', 'var(--shop-item-title-day)');
+    root.style.setProperty(
+      '--shop-radio-background',
+      'var(--shop-radio-background-day)'
+    );
+    root.style.setProperty(
+      '--shop-radio-shadow',
+      'var(--shop-radio-shadow-day)'
+    );
+    root.style.setProperty(
+      '--shop-radio-shadow-one',
+      'var(--shop-radio-shadow-one-day)'
+    );
+    root.style.setProperty(
+      '--shop-radio-shadow-two',
+      'var(--shop-radio-shadow-two-day)'
+    );
 
-    return setLocalStorage();
+    root.style.setProperty(
+      '--about-main-shadow',
+      ' var(--about-main-shadow-day)'
+    );
+  }
+
+  return setLocalStorage();
 }
 
-function loadSavedTheme() { 
-    nightMode = localStorage.getItem('nightMode');
+function loadSavedTheme() {
+  nightMode = localStorage.getItem('nightMode');
 
-    if (localStorage.getItem('nightMode') === 'enabled') {
-        themeSwitch.checked = true;
-        toggleTheme();
-    } 
+  if (localStorage.getItem('nightMode') === 'enabled') {
+    themeSwitch.checked = true;
+    toggleTheme();
+  }
 }
 
 loadSavedTheme();
--- js/shop.js
+++ js/shop.js
@@ -1,20 +1,26 @@
 const COLOR_RADIOS = document.querySelectorAll('input[type="radio"]');
 
 function switchItemColor(e) {
-    const ITEM_COLOR_REGEX = /\[(.*?)\]/g;
-    
-    const radioValue = e.target.value;
-    const srcColorReplacement = `[${radioValue}]`;
-    const closestItemImage = e.target.closest('.item-card').querySelector('.item-img');
-    const currentItemColor = closestItemImage.src.match(ITEM_COLOR_REGEX)[0].slice(1, -1);
+  const ITEM_COLOR_REGEX = /\[(.*?)\]/g;
 
-    if (radioValue === currentItemColor) return; 
-    
-    let newImageSource = closestItemImage.getAttribute('src').replaceAll(ITEM_COLOR_REGEX, srcColorReplacement);
-    
-    closestItemImage.setAttribute('src', newImageSource);
+  const radioValue = e.target.value;
+  const srcColorReplacement = `[${radioValue}]`;
+  const closestItemImage = e.target
+    .closest('.item-card')
+    .querySelector('.item-img');
+  const currentItemColor = closestItemImage.src
+    .match(ITEM_COLOR_REGEX)[0]
+    .slice(1, -1);
+
+  if (radioValue === currentItemColor) return;
+
+  let newImageSource = closestItemImage
+    .getAttribute('src')
+    .replaceAll(ITEM_COLOR_REGEX, srcColorReplacement);
+
+  closestItemImage.setAttribute('src', newImageSource);
 }
 
-COLOR_RADIOS.forEach(radio => {
-    radio.addEventListener('click', switchItemColor);
+COLOR_RADIOS.forEach((radio) => {
+  radio.addEventListener('click', switchItemColor);
 });
